b5352a3d8e8488fd0262266eb22510d73b3ba124
Winnie Ku <winn.ku@gmail.com>
2020-08-04 18:51:26 -0400

    finished media queries and review of scss files

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ idApp.listOfNames = function(results){
 
 // Function for displaying the 10 choices on the DOM 
 idApp.displayChoices = function(array, currentWindow, nextWindow) {
-   idApp.windowBInstructions = `Sweet, now you've got some options to work with! <span class='important'>Choose an identity, and click <span class='button'><i class="fas fa-step-forward" aria-hidden="true"></i><span class="srOnly">Next button</span></span> to find out more about the new you! <br> Don't see anything you like? Hit <span class='button'><i class="fas fa-sync-alt" aria-hidden="true"></i><span class="srOnly">Refresh button</span></span> to refresh the list. We got you.</span>`
+   idApp.windowBInstructions = `Sweet, now you've got some options to work with! <span class='important'>Choose an identity, and click <span class='button'><i class="fas fa-arrow-right" aria-hidden="true"></i><span class="srOnly">Next button</span></span> to find out more about the new you! <br> Don't see anything you like? Hit <span class='button'><i class="fas fa-sync-alt" aria-hidden="true"></i><span class="srOnly">Refresh button</span></span> to refresh the list. We got you.</span>` 
 
    $('.instructions').html(idApp.windowBInstructions);
 
@@ -158,9 +158,9 @@ idApp.displayChoices = function(array, currentWindow, nextWindow) {
    <form action='' class='goBack' id='goBack'>
    </form>
    <div class='buttonContainer'>
-      <button type='submit' form='goBack' title='Back'><i class="fas fa-step-backward" aria-hidden="true"></i><span class="srOnly">Back button</span></button>
+      <button type='submit' form='goBack' title='Back'><i class="fas fa-arrow-left" aria-hidden="true"></i><span class="srOnly">Back button</span></button>
       <button type='submit' form='refreshOptions' id='top' title='Refresh'><i class="fas fa-sync-alt" aria-hidden="true"></i><span class="srOnly">Refresh button</span></button>
-      <button type='submit' form='displayChoices' title='Next'><i class="fas fa-step-forward" aria-hidden="true"></i><span class="srOnly">Next button</span></button>
+      <button type='submit' form='displayChoices' title='Next'><i class="fas fa-arrow-right" aria-hidden="true"></i><span class="srOnly">Next button</span></button>
    </div>
    <div class='errorContainer'>
    </div>
@@ -237,7 +237,7 @@ idApp.finalDisplay = function(array) {
          <form class='backToWindowB' id='backToWindowB'></form>
          <form class='' id='windowCReset'></form>
          <div class='buttonContainer'>
-            <button type='submit' form='backToWindowB' title='Back'><i class="fas fa-step-backward" aria-hidden="true"></i><span class="srOnly">Back button</span></button>
+            <button type='submit' form='backToWindowB' title='Back'><i class="fas fa-arrow-left" aria-hidden="true"></i><span class="srOnly">Back button</span></button>
             <button type='submit' form='windowCReset' title='Reset'><i class="fas fa-redo-alt" aria-hidden="true"></i><span class="srOnly">Reset button</span></button>
          </div>
       </div>
